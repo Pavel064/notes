82a6d54b557af04e2fdebc68e852bb54523a2c7e
Pavel <braurs64@gmail.com>
2022-04-14 10:30:41 +0400

    wip

---
+++ src/Main.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Container, Box } from '@material-ui/core';
+import Editor from './Editor';
+
+const Main = ({ activeNote, onUpdateNote }) => {
+  const onEditField = (field, value) => {
+    onUpdateNote({
+      ...activeNote,
+      [field]: value,
+      lastModified: Date.now(),
+    });
+  };
+
+  if (!activeNote) return <div className="no-active-note">No Active Note</div>;
+
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <React.Fragment>
+          <Container
+            style={{ backgroundColor: '#d4ecff', minHeight: '100vh' }}
+            maxWidth="xl"
+          >
+            <Box p={5}>
+              <Box>
+                <input
+                  type="text"
+                  id="title"
+                  placeholder="Note Title"
+                  value={activeNote.title}
+                  onChange={(e) => onEditField('title', e.target.value)}
+                  autoFocus
+                />
+              </Box>
+              <Box
+                mt={2}
+                style={{
+                  backgroundColor: '#ffffff',
+                  border: '1px solid #cccccc',
+                }}
+              >
+                <Editor />
+              </Box>
+            </Box>
+          </Container>
+        </React.Fragment>
+      </div>
+    </div>
+  );
+};
+export default Main;
